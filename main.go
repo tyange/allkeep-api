ee5eb54300a8eea3d985d96f89d8b6486ff2147b
tyange <usun16@gmail.com>
2024-06-09 18:38:13 +0900

    cors 적용

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/tyange/triplework-backend/db"
 	"github.com/tyange/triplework-backend/routes"
@@ -9,6 +10,11 @@ import (
 func main() {
 	db.InitDB()
 	server := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+
+	server.Use(cors.New(config))
 
 	routes.RegisterRoutes(server)
 
