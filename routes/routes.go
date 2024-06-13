2b747d5957b68af18de64604c4ddce7f9e36debe
tyange <usun16@gmail.com>
2024-06-13 22:34:18 +0900

    add auth middleware

--- routes/routes.go
+++ routes/routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/tyange/white-shadow-api/middlewares"
 )
 
 func RegisterRoutes(server *gin.Engine) {
 	work := server.Group("/work")
+	work.Use(middlewares.Authenticate)
 	work.POST("/save", workSave)
 
 	auth := server.Group("/auth")
